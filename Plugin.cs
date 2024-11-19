d321d3329f74ee18a3d1dc57edb33770f1fa2e8b
Szomti <piotrpiotr1285@gmail.com>
2024-11-19 19:04:33 +0100

    Fix stack changes for other items (like required items)

--- Plugin.cs
+++ Plugin.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 using BepInEx;
 using HarmonyLib;
 using IAmFuture.Data.Items;
+using IAmFuture.Data.SaveLoad;
 using IAmFuture.Data.StorageItems;
+using IAmFuture.Gameplay.Electricity;
+using IAmFuture.Gameplay.Storages;
+using MonoMod.Utils.Cil;
 
 namespace CustomStackSize;
 
@@ -19,8 +26,61 @@ public class Plugin : BaseUnityPlugin
             100f,
             "The custom stack size for every item in the game (MAX: " + GlobalMax + ")");
         CustomGlobalItemStack.MaxStackSize = Math.Abs(globalItemStackSize.Value);
-        new Harmony(MyPluginInfo.PLUGIN_GUID).PatchAll(typeof(CustomGlobalItemStack));
+        var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll(typeof(RestoreStorageTranspiler));
+        harmony.PatchAll(typeof(CustomGlobalItemStack));
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    [HarmonyPatch(typeof(Storage))]
+    internal class RestoreStorageTranspiler
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(Storage), "Restore")]
+        private static IEnumerable<CodeInstruction> Transpiler(
+            IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            var prevConstructor = typeof(ItemStack).GetConstructor([typeof(ItemObject), typeof(float), typeof(float)]);
+            var getIdMethod = typeof(ItemStack).GetMethod("get_ID",
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            var constructor =
+                typeof(ItemStack).GetConstructor([typeof(int), typeof(ItemObject), typeof(float), typeof(float)]);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode != OpCodes.Newobj) continue;
+                if (codes[i].operand as ConstructorInfo != prevConstructor) continue;
+                if (constructor != null)
+                {
+                    codes[i] = new CodeInstruction(OpCodes.Newobj, constructor);
+                }
+
+                int found = 0;
+                for (var j = i; j > 0; j--)
+                {
+                    if (codes[j].opcode != OpCodes.Ldloc_1) continue;
+                    found++;
+                    if (found == 3)
+                    {
+                        var targetLabel = codes[j].labels[0];
+                        codes[j].labels.Remove(targetLabel);
+                        codes.Insert(j, new CodeInstruction(OpCodes.Callvirt, getIdMethod));
+                        codes.Insert(j, new CodeInstruction(OpCodes.Ldloc_1));
+                        codes[j].labels.Add(targetLabel);
+                        break;
+                    }
+                }
+
+                break;
+            }
+
+            foreach (var code in codes)
+            {
+                Console.WriteLine(code.ToString());
+            }
+
+            return codes;
+        }
     }
 
     [HarmonyPatch(typeof(ItemStack))]
@@ -32,7 +92,10 @@ public class Plugin : BaseUnityPlugin
         [HarmonyPatch(typeof(ItemStack), MethodType.Constructor, typeof(int), typeof(ItemObject), typeof(float), typeof(float))]
         private static void Prefix(ref ItemStack __instance, int ID, ItemObject newObject, ref float count, ref float maxCount)
         {
-            maxCount = Math.Min(MaxStackSize, GlobalMax);
+            if (ID >= 0)
+            {
+                maxCount = Math.Min(MaxStackSize, GlobalMax);
+            }
         }
     }
 }
--- obj/Debug/net471/MyPluginInfo.cs
+++ obj/Debug/net471/MyPluginInfo.cs
@@ -4,6 +4,6 @@ namespace CustomStackSize
     {
         public const string PLUGIN_GUID = "CustomStackSize";
         public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "0.1.0";
+        public const string PLUGIN_VERSION = "0.2.0";
     }
 }
